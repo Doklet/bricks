5b9b9ac184b7a54bd487f4b8f7c00be203573470
Enouk <tandy.nilsson@gmail.com>
2015-08-05 10:44:11 +0200

    Only show buttons in edit mode

---
+++ app/scripts/controllers/unauthorized.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bricksApp.controller:UnauthorizedCtrl
+ * @description
+ * # UnauthorizedCtrl
+ * Controller of the bricksApp
+ */
+angular.module('bricksApp')
+  .controller('UnauthorizedCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
